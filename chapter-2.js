4f751d6ae49f009d7e66a5e21fe1e044dc44db9d
Timofiy <Timofeyyevtushenko@gmail.com>
2020-03-26 10:07:34 +0200

    Update chapter-2.js

--- chapter-2.js
+++ chapter-2.js
@@ -2,100 +2,113 @@
 
 // Создайте переменную isMorning со значением true. Поместите переменную в условие оператора if.
 // В теле оператора выполните код console.log("Пора вставать"); Сообщение должно быть выведено в консоль.
-let isMorning = true;
-if (isMorning == true){
-    console.log("Пора вставать");
-}else {console.log("Еще можно спать");}
 // А теперь измените значение isMorning на false и посмотрите что будет.
-
-
 // Задание к уроку 2.3
 // Добавьте в предыдущий код оператор else. В теле оператора выполните код console.log("Еще можно спать");
-// Задание к уроку 2.4
+let isMorning = false;
+if (isMorning === true) {
+    console.log("Пора вставать");
+} else {
+    console.log("Еще можно спать");
+}
 
+
+// Задание к уроку 2.4
 
 // У нас есть 9 этажное здание. Наш офис находится на 7м этаже.
 // Напишите программу, используя if и операторы сравнения, которая говорит, если лифт на 7 этаже,
+// это наш этаж и нам пора выходить.
 let currentLevel = 6;
-if(currentLevel == 7){
+if (currentLevel === 7) {
     console.log("это наш этаж и нам пора выходить.");
 }
-// это наш этаж и нам пора выходить.
+
+
 // Задание к уроку 2.5
 
 // Напишите программу, используя if..else и условные операторы. Суть такова: если вы сдали экзамен и вам уже
 // исполнилось 18 – вы получаете водительское удостоверение, иначе катайтесь на велосипеде
-
 let age = 18;
 let isExamPassed = true;
 let driversLicense = false;
-if(age >= 18 && isExamPassed === true){
+if (age >= 18 && isExamPassed === true) {
     console.log("вы получаете водительское удостоверение");
-    let driversLicense = true;
-}else {console.log("катайтесь на велосипеде");
+    driversLicense = true;
+} else {
+    console.log("катайтесь на велосипеде");
 }
 
 
 // Задание к уроку 2.6
-let applesInTheBag = 10;
-let isImHungry = true;
-if (applesInTheBag > 0){
-    isImHungry = false;
-    console.log("isImHungry "+isImHungry);
-}else {console.log("isImHungry "+isImHungry);}
+
 // Нашишите программу, которая гласит: если у меня в корзине 0 яблок, значит я голоден. Иначе – я сыт.
+let applesInTheBag = -3;
+if (applesInTheBag>0) {
+    console.log("я сыт");
+} else {
+    console.log("у меня в корзине 0 яблок, значит я голоден");
+}
 // Нашишите программу, которая принимает переменную answer типа string, и гласит: если есть ответ, я получил работу
-let answer = "";
-if (answer){console.log("I got a work!")}else
-{console.log("Waiting for response..")}
 // иначе – ожидаю ответ.
+let answer = "";
+if (answer) {
+    console.log("я получил работу");
+} else {
+    console.log("ожидаю ответ");
+}
+
 
 // Задание к уроку 2.7
 
-let tool = 'marker';
 // Используйте короткую запись сравнения, присвойте в переменную myTool
 // значение переменной tool, иначе 'pen'
-let myTool = '';
-myTool += tool|| `pen`;
+// Перепишите значение переменной tool на 'marker', и посмотрите на результат снова.
+let tool = 'marker';
+let myTool = tool || `pen`;
 console.log(`My tool is ${myTool}.`);
 
-// Перепишите значение переменной tool на 'marker', и посмотрите на результат снова.
 
 // Задание к уроку 2.8
+
 // Приведите все выражения ниже к тернарному оператору
 let isLocked = false;
-isLocked ? console.log('Тебе нужен ключ.'):console.log('Ключ не требуется.');
+isLocked ? console.log('Тебе нужен ключ.') : console.log('Ключ не требуется.');
 
 let isCorrect = true;
-isCorrect ? console.log('Правильно!') :  console.log('Неправильно!');
+isCorrect ? console.log('Правильно!') : console.log('Неправильно!');
 
 let myMoto = 'Share your knowledge!';
 myMoto === 'Share your knowledge' ?  console.log('I teach on Smartians!') : console.log("I study on Smartians!");
 
+
 // Задание к уроку 2.9
+
 // Создай переменную today и присвойте ей значение сегодняшнего дня недели.
 // Затем с помощью else if сделайте вывод в консоль, какой сегодня день недели.
 let today = `Wednesday`;
-if (today == `Monday`) {
-    console.log(`Today is ${today}`)
-}else if (today == `Tuesday`){
-    console.log(`Today is ${today}`)
-}else if (today == `Wednesday`){
-    console.log(`Today is ${today}`)
-}else if (today == `Thursday`){
-    console.log(`Today is ${today}`)
-}else if (today == `Friday`){
-    console.log(`Today is ${today}`)
-}else if (today == `Saturday`){
-    console.log(`Today is ${today}`)
-}else if (today == `Sunday`){
-    console.log(`Today is ${today}`)
-}else{
-    console.log(`uncorrect data`)
+if (today === `Monday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Tuesday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Wednesday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Thursday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Friday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Saturday`) {
+    console.log(`Today is ${today}`);
+} else if (today === `Sunday`) {
+    console.log(`Today is ${today}`);
+} else {
+    console.log(`uncorrect data`);
 }
+
+
 // Задание к уроку 2.10
+
 // Выполните задание 2.9 еще раз, используя Switch
-switch (today){
+switch (today) {
     case 'Monday':
         console.log(`Today is ${today}`);
         break;
