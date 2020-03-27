3ffa2841150854b1fde8120239c15163c257ddcb
TimonHC <timofeyyevtushenko@gmail.com>
2020-03-27 11:32:37 +0200

    completed chapter 3 exercise

---
+++ chapter-3.js
@@ -0,0 +1,22 @@
+//Напишите функцию min, принимающую два аргумента, и возвращающую минимальный из них.
+
+function min (arg1, arg2){
+    let minimal = arg1;
+    if (arg2<minimal) {
+        minimal = arg2;
+    }
+    return minimal;
+}
+console.log(min (10, 5));
+
+//Напиши функцию-счётчик, которая считает свои вызовы и возвращает их текущее число.
+var iterator = 0;
+function counter() {
+    iterator++;
+    console.log(iterator + ' calls of function');
+    return iterator;
+}
+counter();
+counter();
+counter();
+
