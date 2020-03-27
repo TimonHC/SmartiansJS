19dab2c7f27ca5f3845096ffff95676590d2746a
TimonHC <timofeyyevtushenko@gmail.com>
2020-03-27 11:44:04 +0200

    refactored chapter-3

--- chapter-3.js
+++ chapter-3.js
@@ -1,13 +1,17 @@
 //Напишите функцию min, принимающую два аргумента, и возвращающую минимальный из них.
 
-function min (arg1, arg2){
+/*function min (arg1, arg2){
     let minimal = arg1;
-    if (arg2<minimal) {
+    if (arg2 < minimal) {
         minimal = arg2;
     }
     return minimal;
 }
 console.log(min (10, 5));
+*/
+let minNumber = (arg1, arg2) => console.log(`min is ` + Math.min(arg1, arg2));
+minNumber(4,1);
+
 
 //Напиши функцию-счётчик, которая считает свои вызовы и возвращает их текущее число.
 var iterator = 0;
