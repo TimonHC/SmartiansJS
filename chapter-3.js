7642a057b942e7b3259cde3e2bc3723af8fff59f
TimonHC <timofeyyevtushenko@gmail.com>
2020-03-30 15:27:18 +0300

    refactored chapter-3

--- chapter-3.js
+++ chapter-3.js
@@ -6,15 +6,17 @@ console.log('min number is ' + minNumber(1, 5));
 
 //Напиши функцию-счётчик, которая считает свои вызовы и возвращает их текущее число.
 //И сделай счетчик не глобальным, а локальным.
+
 function counter() {
     let iterator = 0;
-        return () => console.log(++iterator + ' calls of function');
+        return () => ++iterator;
 }
 
-var makeCounter = counter();
-makeCounter();
-makeCounter();
-makeCounter();
+let makeCounter = counter();
+console.log(makeCounter() + ' calls of function');
+console.log(makeCounter() + ' calls of function');
+console.log(makeCounter() + ' calls of function');
+
 
 //Напиши для практики функцию, которая выводит наибольшее число.
 let maxNumber = (arg1, arg2) => arg1 > arg2 ? console.log('max number is ' + arg1) : console.log('max number is ' + arg2);
