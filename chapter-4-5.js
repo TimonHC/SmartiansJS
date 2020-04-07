079f330bdd9a5454fd3b57b307d6576c2b05a006
TimonHC <timofeyyevtushenko@gmail.com>
2020-04-07 13:14:33 +0300

    job check request

--- chapter-4-5.js
+++ chapter-4-5.js
@@ -1,24 +1,27 @@
 /*Треугольник
 Напишите цикл, который выводит в консоль треугольник*/
-
+let triangle = '';
+for (i = 0; i < 5; i++) {
+    console.log(triangle += ' #');
+}
 //console.log(triangle);
 
 /*Шахматная доска
 Напиши программу, создающую строку, содержащую решётку 8х8, в которой линии разделяются символами новой строки.
 На каждой позиции либо пробел, либо #. В результате должна получиться шахматная доска.*/
-let grid = '';
 
-for (i = 0; i!==8; ++i) {
+let grid = '';
+for (i = 0; i < 8; ++i) {
     if (i % 2 === 0) {
         grid += '\n';
         for (j = 0; j < 4; j++) {
             grid += '#';
-            grid += ' ';
+            grid += '@';
         }
     } else {
         grid += '\n';
         for (j = 0; j < 4; j++) {
-            grid += ' ';
+            grid += '@';
             grid += '#';
         }
     }
@@ -35,13 +38,14 @@ console.log(reverseArray(["A", "B", "C"]));
 function reverseArray(arr) {
     newArr = [];
     j=0;
-    for (i = arr.length-1; i!==-1; i--){
+    for (i = arr.length-1; i>-1; i--) {
         newArr[j] = arr[i];
         j++;
     }
     return newArr;
 }
 console.log(reverseArray(["A", "B", "C"]));
+
 /*
 Високосный год
 Напиши функцию, которая принимает в себя год и выдаёт ответ, високосный он или нет*/
