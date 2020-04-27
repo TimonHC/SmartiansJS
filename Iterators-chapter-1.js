3a6441e1727316cc89dbb0ef6b5a4f570c6cea4b
TimonHC <timofeyyevtushenko@gmail.com>
2020-04-27 17:45:50 +0300

    refactored & completed iterators

--- Iterators-chapter-1.js
+++ Iterators-chapter-1.js
@@ -1,43 +1,49 @@
+/**
+ * Performs the specified action for each element in an array.
+ * @param callbackfn  A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+ * @param thisArg  An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+ */
+//forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
+/**
 /*
 forEach()
 1. Дан массив с числами(сам придумай). Создай новый массив, состоящий из квадратов этих чисел.
 */
 const numbers = [1, -2, 3, -4, 5, -6];
 const squaredNumbers = [];
-numbers.forEach(numbersItem =>
-    squaredNumbers.push(Math.pow(numbersItem, 2))
-);
+numbers.forEach(number =>
+    squaredNumbers.push(number, 2));
 console.log(squaredNumbers);
 /*
 2. Дан массив с числами. Найди сумму этих чисел.
 */
 let sumOfTheNumbers = 0;
-numbers.forEach(numbers =>
-    sumOfTheNumbers += numbers);
+numbers.forEach(number =>
+    sumOfTheNumbers += number);
 console.log(sumOfTheNumbers);
 /*
 map()
 3. Дан массив с числами. Преобразуй в массив, состоящий из квадратов этих чисел
 */
-const squaredNumbers2 = numbers.map(numbers =>
-    Math.pow(numbers, 2));
+const squaredNumbers2 = numbers.map(number =>
+    Math.pow(number, 2));
 console.log(squaredNumbers2);
 /*
 every(), some()
 4. Дан массив с числами. Проверь то, что все элементы в массиве больше нуля.
 5. Дан массив с числами. Проверь то, что в нем есть отрицательные элементы.
 */
-const isEveryNumBiggerThanZero = numbers.every(numbers =>
-    numbers > 0);
+const isEveryNumBiggerThanZero = numbers.every(number =>
+    number > 0);
 console.log(isEveryNumBiggerThanZero);
-const isAnyNumLessThanZero = numbers.some(numbers =>
-    numbers < 0);
+const isAnyNumLessThanZero = numbers.some(number =>
+    number < 0);
 console.log(isAnyNumLessThanZero);
 /*
 filter()
 6. Дан массив с числами. Оставь в нем только отрицательные числа*!/
 *!/
 */
-const NumsAreGreaterThanZero = numbers.filter(numbers =>
-    numbers >= 0);
-console.log(NumsAreGreaterThanZero);
+const NumsAreLessThanZero = numbers.filter(number =>
+    number < 0);
+console.log(NumsAreLessThanZero);
