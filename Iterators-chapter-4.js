05b13b0771080a0d08789f27f314a16d55e6120d
TimonHC <timofeyyevtushenko@gmail.com>
2020-04-30 12:39:46 +0300

    iterators chapter 4 for check

---
+++ Iterators-chapter-4.js
@@ -0,0 +1,16 @@
+/*
+15. Дан массив с числами. Узнай сколько элементов с конца массива надо сложить, чтобы в сумме получилось больше 10-ти.
+*/
+let numbers = [-1, -2, -3, 0, 4, 5, 6];
+let countOfElements = 0;
+numbers.reduceRight((sum, current, index) => {
+if(sum < 10) countOfElements = numbers.length - index;
+return sum + current;
+}, 0);
+console.log(countOfElements);
+/*
+    И последнее, выбирай любой
+16. Дан массив с числами. Оставь в нем только положительные числа. Затем извлеките квадратный корень и этих чисел.
+*/
+let result = numbers.filter(number => (number > 0)).map(number => Math.sqrt(number));
+console.log(result);
