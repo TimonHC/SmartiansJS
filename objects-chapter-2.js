2cf1106e78a6b48dc3bca8f033ca721be66b19a3
Alexander <a.kabanov.pz@gmail.com>
2020-05-26 20:33:02 +0300

    Added task body

---
+++ objects-chapter-2.js
@@ -0,0 +1,39 @@
+// Рассмотрим такой, достаточно надуманный, объект:
+var box = {
+    locked: true,
+    unlock: function() { this.locked = false; },
+    lock: function() { this.locked = true;  },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Заперто!");
+        return this._content;
+    }
+};
+
+// Это коробочка с замком. Внутри лежит массив, но до него можно добраться только, когда коробка не заперта.
+// Напрямую обращаться к свойству _content нельзя.
+//
+// Напишите функцию withBoxUnlocked, принимающую в качестве аргумента функцию, которая отпирает коробку,
+// выполняет функцию, и затем обязательно запирает коробку снова перед выходом – неважно, выполнилась ли
+// переданная функция правильно, или она выбросила исключение.
+
+function withBoxUnlocked(body) {
+    // Пиши свой код здесь
+}
+
+withBoxUnlocked(function() {
+    box.content.push("золотишко");
+});
+
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Пираты на горизонте! Отмена!");
+    });
+} catch (e) {
+    console.log("Произошла ошибка:", e);
+}
+console.log(box.locked);
+// → true
+
+// В качестве бонуса убедитесь, что при вызове withBoxUnlocked, когда коробка не заперта,
+// коробка остаётся незапертой.
