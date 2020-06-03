24b2ab49167b62f659f984bb62b761758bc9b1a1
TimonHC <timofeyyevtushenko@gmail.com>
2020-06-03 15:58:11 +0300

    refactored obj-chapter-2-for-check

--- objects-chapter-2.js
+++ objects-chapter-2.js
@@ -16,16 +16,16 @@ var box = {
 // Напишите функцию withBoxUnlocked, принимающую в качестве аргумента функцию, которая отпирает коробку,
 // выполняет функцию, и затем обязательно запирает коробку снова перед выходом – неважно, выполнилась ли
 // переданная функция правильно, или она выбросила исключение.
-box.unlock();
-function withBoxUnlocked(body) {
-    if (box.locked) {
+//box.unlock();
+let withBoxUnlocked = function(body) {
+    if (box.locked){
         box.unlock();
-        body;
-        box.lock();} else {
-        body;
+        body();
+        box.lock();
     }
+    body();
+};
 
-}
 
 withBoxUnlocked(function() {
     box.content.push("золотишко");
@@ -39,6 +39,9 @@ try {
     console.log("Произошла ошибка:", e);
 }
 console.log(box.locked);
+box.unlock();
+console.log(box.content);
+box.lock();
 // → true
 
 // В качестве бонуса убедитесь, что при вызове withBoxUnlocked, когда коробка не заперта,
