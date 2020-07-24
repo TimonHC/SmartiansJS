93e22a1049768436a9363f6b339f5274636b7ec4
TimonHC <timofeyyevtushenko@gmail.com>
2020-07-24 18:41:51 +0300

    Добавил функции создающие массивы координат для атак ИИ и игрока, изменил условия победы - потопить 20 палуб, удалил поля, которые не используются при таком решении

--- NavalBattle.js
+++ NavalBattle.js
@@ -26,22 +26,14 @@ enemyShips = [1, 2, 3, 4, 9, 19, 29, 31, 32, 33, 52, 55, 82, 85, 79, 80, 87, 97,
 myShips = [4, 5, 6, 8, 9, 10, 12, 22, 24, 29, 32, 42, 64, 69, 84, 94, 86, 96, 89, 99];
 myMines = [25, 28, 55, 58];
 enemyMines = [21, 39, 69, 96];
-mockableAttackCoords = [1, 3, 4];
-aiDefaultAttackCoords =
-    [3, 4, 7, 8, 13, 14, 17, 18, 21, 22, //дефолтные координаты по которым максимум за 24 хода найдется четырехпалубный
-    25, 26, 29, 30, 31, 32, 35, 36, 39,
-    40, 43, 44, 47, 48, 53, 54, 57, 58,
-    61, 62, 65, 66,69, 70, 71, 72, 75,
-    76, 79, 80, 83, 84, 87, 88, 93, 94, 97, 98];
+mockableAttackCoords = [];
+aiDefaultAttackCoords = [];
 aiOtherAttackCoords = []; //все, кроме aiDefaultAttackCoords
-aiLastSuccessAttackCoords = 0; //память, чтоб держать прошлое попадание
-
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-
 isPlayer = true; //право первого хода
 myHitsCounter = 0; //счетчик победы
 enemieHitsCounter = 0; //счетчик победы
@@ -51,6 +43,12 @@ function fillField(field) {
     }
     return field;
 } //заполняет игровое поле "@"
+function setAttackCoords(field){
+    for (let i = 1; i <= 100; i++) {
+        field.push(i);
+    }
+    field.sort(() => Math.random() - 0.5);
+} //заполняет массив координат атаки числами [1;100] и перемешивает их
 function putMinesOnField(field, minesCoords) {
     for (let i = 0; i < minesCoords.length; i++) {
         field[minesCoords[i]-1] = '+';
@@ -73,6 +71,11 @@ function shipExplodesOnMine(field){
         myHitsCounter++;
     }
 } //функция обработки события попадания в мину
+function getCoordsForAttack(coords){
+    let result = coords[0];
+    coords.splice(0, 1);
+    return result;
+} // функция возвращающая координаты для хода ИИ, так-же удаляет их из массива
 function attack(field, coords) {
     switch (field[coords-1]){
 /*        Условные обозначения:
@@ -155,17 +158,17 @@ function initFields(myField, enemyField, guessField,
 } //функция инициализации полей
 
 function initGame() {
+    setAttackCoords(mockableAttackCoords);
+    setAttackCoords(aiDefaultAttackCoords);
     initFields(myField, enemyField, guessField, myShips, enemyShips, myMines, enemyMines);
-    printField(enemyField);
 
     //счетчик для прохождения по массивам координат атаки
-    let i = 0;
-    while ((myHitsCounter || enemieHitsCounter) !== 2 ) {
-        console.log(mockableAttackCoords[i]);
-        makeTurn(mockableAttackCoords[i]);
-        i++;
+    while ((myHitsCounter || enemieHitsCounter) !== 20 ) {
+        if (isPlayer) {
+            makeTurn(getCoordsForAttack(mockableAttackCoords));
+        } else {
+makeTurn(getCoordsForAttack(aiDefaultAttackCoords));}
     }
-
     myHitsCounter > enemieHitsCounter ? console.log('player win') : console.log('enemy win');
 }
 
